e3a9ccb6180235fdbe021697a89da841b0736c81
nxtdaydelivery <79922345+nxtdaydelivery@users.noreply.github.com>
2021-12-21 13:43:13 +0000

    update progress

--- src/main/java/w/overflow/tabulous/config/TabulousConfig.java
+++ src/main/java/w/overflow/tabulous/config/TabulousConfig.java
@@ -8,6 +8,7 @@ import w.overflow.tabulous.Tabulous;
 import w.overflow.tabulous.updater.DownloadGui;
 import w.overflow.tabulous.updater.Updater;
 
+import java.awt.*;
 import java.io.File;
 
 public class TabulousConfig extends Vigilant {
@@ -18,39 +19,142 @@ public class TabulousConfig extends Vigilant {
             category = "General"
     )
     public static boolean modEnabled = true;
-
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Toggle",
+            description = "Enable tab toggling open and closed.",
+            category = "General", subcategory = "General"
+    )
+    public static boolean toggle = false;
 
 
     @Property(type = PropertyType.SELECTOR,
             name = "Tab Styling", description = "Styling to use when rendering the tab menu.",
-            category = "Tab", options = {"Vanilla", "Vanilla+", "Custom"})
-    public static int strategy = 0;
+            category = "Tab", options = {"Vanilla-Style", "Vanilla+ (Animated)", "Tabulous (Reimagined Gui)", "Tab Disabled"})
+    public static int strategy = 1;
 
     @Property(
             type = PropertyType.SWITCH,
             name = "Show Header",
-            description = "Show the header on the scoreboard.",
+            description = "Show the header on the tab menu.",
             category = "Tab", subcategory = "Headers/Footers"
     )
     public static boolean showHeader = true;
     @Property(
             type = PropertyType.SWITCH,
             name = "Show Footer",
-            description = "Show the footer on the scoreboard.",
+            description = "Show the footer on the tab menu.",
             category = "Tab", subcategory = "Headers/Footers"
     )
     public static boolean showFooter = true;
+    @Property(
+            type = PropertyType.SLIDER,
+            name = "Top Position",
+            description = "Position of the top of the tab menu. (default: 10)\nSet it to 0 to be seamless with the top of the screen.",
+            category = "Tab", subcategory = "General", max = 20
+    )
+    public static int topPosition = 10;
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Show Ping",
+            description = "Show the ping values on the tab menu.",
+            category = "Tab", subcategory = "Ping"
+    )
+    public static boolean renderPing = true;
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Show Ping Numbers",
+            description = "Show the ping values as numbers instead on the tab menu.\nThis will replace the normal ping render method, so don't try having Show Ping on as well. It won't work.",
+            category = "Tab", subcategory = "Ping"
+    )
+    public static boolean renderPingNums = false;
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Show Scoreboard Values",
+            description = "Show the extended scoreboard values in the tab menu. \nNote: Not really used on Hypixel, but is on other servers.",
+            category = "Tab", subcategory = "General"
+    )
+    public static boolean renderScoreboardValues = true;
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Force Hide NPCs",
+            description = "Force hide NPCs from the tab list on Hypixel.",
+            category = "Tab", subcategory = "Hypixel"
+    )
+    public static boolean hideNPCs = true;
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Don't Show Heads",
+            description = "Don't show the head of the player in the tab menu.",
+            category = "Tab", subcategory = "Heads"
+    )
+    public static boolean dontShowHeads = false;
+    @Property(
+            type = PropertyType.SELECTOR,
+            name = "Head Position",
+            description = "Which side to show the head on in the tab menu.",
+            category = "Tab", subcategory = "Heads",
+            options = {"Left", "Right"}
+    )
+    public static int headPos = 0;
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Hide Guild Tags",
+            description = "Hide the guild tags shown in tab on Hypixel.",
+            category = "Tab", subcategory = "Hypixel"
+    )
+    public static boolean hideGuilds = false;
+    @Property(
+            type = PropertyType.SLIDER,
+            name = "Overflow Amount",
+            description = "Amount of names to show before splitting into multiple columns. (default: 20)",
+            category = "Tab", subcategory = "General", min = 5, max = 40
+    )
+    public static int overflow = 20;
 
 
-
-
+    @Property(
+            type = PropertyType.CHECKBOX,
+            name = "Custom Tab Options",
+            description = "These settings are for tab customization, including colors, animations, and more.\n\u00A7eThese settings only take effect on the Tabulous mode!",
+            category = "Tabulous Customization"
+    )
+    public static boolean customTab = true;
+    @Property(
+            type = PropertyType.PARAGRAPH,
+            name = "Custom Header Text",
+            description = "Text for the custom header. Supports Minecraft color codes.\nSet to 'default' to disable.",
+            category = "Tabulous Customization", subcategory = "Headers/Footers"
+    )
+    public static String headerText = "default";
+    @Property(
+            type = PropertyType.PARAGRAPH,
+            name = "Custom Footer Text",
+            description = "Text for the custom footer. Supports Minecraft color codes.\nSet to 'default' to disable.",
+            category = "Tabulous Customization", subcategory = "Headers/Footers"
+    )
+    public static String footerText = "default";
+    @Property(
+            type = PropertyType.COLOR,
+            name = "Tab Color",
+            description = "Color for the tab menu.",
+            category = "Tabulous Customization", subcategory = "Colors"
+    )
+    public static Color tabColor = new Color(50, 50, 50, 100);
+    @Property(
+            type = PropertyType.COLOR,
+            name = "Tab Name Color",
+            description = "Color for the names of people in tab.",
+            category = "Tabulous Customization", subcategory = "Colors"
+    )
+    public static Color tabNameColor = new Color(50, 50, 50, 200);
 
 
     @Property(
             type = PropertyType.SWITCH,
             name = "Show Update Notification",
             description = "Show a notification when you start Minecraft informing you of new updates.",
-            category = "Updater"
+            category = "Updates & Support", subcategory = "Updater"
     )
     public static boolean showUpdate = true;
 
@@ -58,7 +162,7 @@ public class TabulousConfig extends Vigilant {
             type = PropertyType.BUTTON,
             name = "Update Now",
             description = "Update by clicking the button.",
-            category = "Updater"
+            category = "Updates & Support", subcategory = "Updater"
     )
     public void update() {
         if (Updater.shouldUpdate) EssentialAPI.getGuiUtil()
--- src/main/java/w/overflow/tabulous/mixin/GuiPlayerTabOverlayMixin.java
+++ src/main/java/w/overflow/tabulous/mixin/GuiPlayerTabOverlayMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.entity.player.EnumPlayerModelParts;
 import net.minecraft.scoreboard.IScoreObjectiveCriteria;
 import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.world.WorldSettings;
@@ -30,13 +31,18 @@ import java.util.List;
 
 @Mixin(GuiPlayerTabOverlay.class)
 public class GuiPlayerTabOverlayMixin {
+    private float percentComplete = 0f;
+    private boolean retract = false;
+    private boolean animations;
 
     @Final
     @Shadow
-    private final Minecraft mc = Minecraft.getMinecraft();
+    private Minecraft mc;
+
     @Final
     @Shadow
     private static Ordering<NetworkPlayerInfo> field_175252_a;
+
     @Shadow
     private IChatComponent footer;
     @Shadow
@@ -88,36 +94,63 @@ public class GuiPlayerTabOverlayMixin {
 
     @Inject(method = "renderPlayerlist", at = @At("HEAD"), cancellable = true)
     public void renderPlayerlist(int width, Scoreboard scoreboardIn, ScoreObjective scoreObjectiveIn, CallbackInfo ci) {
-        if(TabulousConfig.modEnabled) {
+        if (TabulousConfig.modEnabled) {
             switch (TabulousConfig.strategy) {
-                default:
                 case 0:
-                    renderVanilla(width, scoreboardIn, scoreObjectiveIn);
+                    animations = false;
+                    renderVanillaPlus(width, scoreboardIn, scoreObjectiveIn);
+                    ci.cancel();
                     break;
+                default:
                 case 1:
+                    animations = true;
+                    renderVanillaPlus(width, scoreboardIn, scoreObjectiveIn);
+                    ci.cancel();
                     break;
                 case 2:
+                    renderCustom(width, scoreboardIn, scoreObjectiveIn);
+                    ci.cancel();
                     break;
-
+                case 3:
+                    ci.cancel();
+                    break;
             }
-            ci.cancel();
         }
-        //System.out.println("fin");
+    }
+
+
+    private static float clamp(float number) {
+        return number < (float) 0.0 ? (float) 0.0 : Math.min(number, (float) 1.0);
+    }
+
+    private static float easeOut(float current, float goal) {
+        if (Math.floor(Math.abs(goal - current) / (float) 0.01) > 0) {
+            return current + (goal - current) / (float) 15.0;
+        } else {
+            return goal;
+        }
     }
 
 
     /**
      * vanilla method for drawing the scoreboard (with custom features)
+     *
      * @param width            width
      * @param scoreboardIn     current scoreboard
      * @param scoreObjectiveIn current scoreboard objectives
      */
-    public void renderVanilla(int width, Scoreboard scoreboardIn, ScoreObjective scoreObjectiveIn) {
-        if(!TabulousConfig.showHeader) header = null;
-        if(!TabulousConfig.showFooter) footer = null;
+    public void renderVanillaPlus(int width, Scoreboard scoreboardIn, ScoreObjective scoreObjectiveIn) {
+        if (!TabulousConfig.showHeader) header = null;
+        if (!TabulousConfig.showFooter) footer = null;
 
         NetHandlerPlayClient nethandlerplayclient = mc.thePlayer.sendQueue;
         List<NetworkPlayerInfo> list = getPlayerInfoOrdering().sortedCopy(nethandlerplayclient.getPlayerInfoMap());
+        if (TabulousConfig.hideNPCs) {
+            try {
+                list.removeIf(info -> getPlayerName(info).startsWith("\u00A78[NPC]") || !getPlayerName(info).startsWith("\u00A7"));
+            } catch (Exception ignored) {
+            }
+        }
         int i = 0;
         int j = 0;
         for (NetworkPlayerInfo networkplayerinfo : list) {
@@ -135,11 +168,12 @@ public class GuiPlayerTabOverlayMixin {
         int i4 = l3;
         int j4;
 
-        for (j4 = 1; i4 > 20; i4 = (l3 + j4 - 1) / j4) {
+        for (j4 = 1; i4 > TabulousConfig.overflow; i4 = (l3 + j4 - 1) / j4) {
             ++j4;
         }
 
         boolean flag = mc.isIntegratedServerRunning() || mc.getNetHandler().getNetworkManager().getIsencrypted();
+        if (TabulousConfig.dontShowHeads) flag = false;
         int l;
 
         if (scoreObjectiveIn != null) {
@@ -154,7 +188,7 @@ public class GuiPlayerTabOverlayMixin {
 
         int i1 = Math.min(j4 * ((flag ? 9 : 0) + i + l + 13), width - 50) / j4;
         int j1 = width / 2 - (i1 * j4 + (j4 - 1) * 5) / 2;
-        int k1 = 10;
+        int k1 = TabulousConfig.topPosition;
         int l1 = i1 * j4 + (j4 - 1) * 5;
         List<String> list1 = null;
         List<String> list2 = null;
@@ -175,6 +209,7 @@ public class GuiPlayerTabOverlayMixin {
             }
         }
 
+        // RENDER HEADER
         if (list1 != null) {
             Gui.drawRect(width / 2 - l1 / 2 - 1, k1 - 1, width / 2 + l1 / 2 + 1, k1 + list1.size() * mc.fontRendererObj.FONT_HEIGHT, Integer.MIN_VALUE);
 
@@ -205,27 +240,39 @@ public class GuiPlayerTabOverlayMixin {
                 String s1 = getPlayerName(networkplayerinfo1);
                 GameProfile gameprofile = networkplayerinfo1.getGameProfile();
 
+                // RENDER HEAD
                 if (flag) {
                     EntityPlayer entityplayer = mc.theWorld.getPlayerEntityByUUID(gameprofile.getId());
                     boolean flag1 = entityplayer != null && entityplayer.isWearing(EnumPlayerModelParts.CAPE) && (gameprofile.getName().equals("Dinnerbone") || gameprofile.getName().equals("Grumm"));
                     mc.getTextureManager().bindTexture(networkplayerinfo1.getLocationSkin());
                     int l2 = 8 + (flag1 ? 8 : 0);
                     int i3 = 8 * (flag1 ? -1 : 1);
-                    Gui.drawScaledCustomSizeModalRect(j2, k2, 8.0F, (float) l2, 8, i3, 8, 8, 64.0F, 64.0F);
+                    int headPos = j2;
+                    if (TabulousConfig.headPos == 1) {
+                        System.out.println("hi");
+                        headPos = j2 + i1 - 8;
+                    }
+                    Gui.drawScaledCustomSizeModalRect(headPos, k2, 8.0F, (float) l2, 8, i3, 8, 8, 64.0F, 64.0F);
 
                     if (entityplayer != null && entityplayer.isWearing(EnumPlayerModelParts.HAT)) {
                         int j3 = 8 + (flag1 ? 8 : 0);
                         int k3 = 8 * (flag1 ? -1 : 1);
-                        Gui.drawScaledCustomSizeModalRect(j2, k2, 40.0F, (float) j3, 8, k3, 8, 8, 64.0F, 64.0F);
+                        Gui.drawScaledCustomSizeModalRect(headPos, k2, 40.0F, (float) j3, 8, k3, 8, 8, 64.0F, 64.0F);
                     }
 
-                    j2 += 9;
+                    if (TabulousConfig.headPos == 0) j2 += 9;
                 }
 
+                // RENDER NAMES
                 if (networkplayerinfo1.getGameType() == WorldSettings.GameType.SPECTATOR) {
                     s1 = EnumChatFormatting.ITALIC + s1;
                     mc.fontRendererObj.drawStringWithShadow(s1, (float) j2, (float) k2, -1862270977);
                 } else {
+                    if (TabulousConfig.hideGuilds) {
+                        if (s1.charAt(s1.length() - 1) == ']') {
+                            s1 = s1.substring(0, s1.lastIndexOf("\u00A7"));
+                        }
+                    }
                     mc.fontRendererObj.drawStringWithShadow(s1, (float) j2, (float) k2, -1);
                 }
 
@@ -234,14 +281,36 @@ public class GuiPlayerTabOverlayMixin {
                     int l5 = k5 + l;
 
                     if (l5 - k5 > 5) {
-                        renderScoreboardValues(scoreObjectiveIn, k2, gameprofile.getName(), k5, l5, networkplayerinfo1);
+                        if (TabulousConfig.renderScoreboardValues) {
+                            renderScoreboardValues(scoreObjectiveIn, k2, gameprofile.getName(), k5, l5, networkplayerinfo1);
+                        }
                     }
                 }
-
-                renderPing(i1, j2 - (flag ? 9 : 0), k2, networkplayerinfo1);
+                if (TabulousConfig.renderPingNums && TabulousConfig.headPos != 1) {
+                    String pingNum = "\u00A7";
+                    int ping = networkplayerinfo1.getResponseTime();
+                    if (ping != 1) {
+                        if (ping < -1) pingNum += "5";
+                        else if (ping < 100) {
+                            pingNum += "2";
+                        } else if (ping < 300) {
+                            pingNum += "a";
+                        } else if (ping < 400) {
+                            pingNum += "e";
+                        } else {
+                            pingNum += "c";
+                        }
+                        pingNum += String.valueOf(networkplayerinfo1.getResponseTime());
+                        mc.fontRendererObj.drawStringWithShadow(pingNum, i1 + j2 - (flag ? 9 : 0) - mc.fontRendererObj.getStringWidth(pingNum), k2, -1);
+                    }
+                    return;
+                }
+                if (TabulousConfig.renderPing && TabulousConfig.headPos != 1)
+                    renderPing(i1, j2 - (flag ? 9 : 0), k2, networkplayerinfo1);
             }
         }
 
+        // RENDER FOOTER
         if (list2 != null) {
             k1 = k1 + i4 * 9 + 1;
             Gui.drawRect(width / 2 - l1 / 2 - 1, k1 - 1, width / 2 + l1 / 2 + 1, k1 + list2.size() * mc.fontRendererObj.FONT_HEIGHT, Integer.MIN_VALUE);
@@ -254,4 +323,192 @@ public class GuiPlayerTabOverlayMixin {
         }
     }
 
+    /**
+     * Method for drawing the scoreboard with animations and color customization options. fully custom + de-obfuscated.
+     * @param width            screen width
+     * @param scoreboardIn     current scoreboard
+     * @param scoreObjectiveIn current scoreboard objectives
+     */
+    public void renderCustom(int width, Scoreboard scoreboardIn, ScoreObjective scoreObjectiveIn) {
+        if (!TabulousConfig.showHeader) header = null;
+        if (!TabulousConfig.headerText.equals("default")) {
+            header = new ChatComponentText(TabulousConfig.headerText);
+        }
+        if (!TabulousConfig.showFooter) footer = null;
+        if (!TabulousConfig.footerText.equals("default")) {
+            footer = new ChatComponentText(TabulousConfig.footerText);
+        }
+        NetHandlerPlayClient nethandlerplayclient = mc.thePlayer.sendQueue;
+        List<NetworkPlayerInfo> list = getPlayerInfoOrdering().sortedCopy(nethandlerplayclient.getPlayerInfoMap());
+        if (TabulousConfig.hideNPCs) {
+            try {
+                list.removeIf(info -> getPlayerName(info).startsWith("\u00A78[NPC]") || !getPlayerName(info).startsWith("\u00A7"));
+            } catch (Exception ignored) {}
+        }
+        int nameLength = 0;
+        int objWidth = 0;
+        for (NetworkPlayerInfo networkplayerinfo : list) {
+            int stringWidth = mc.fontRendererObj.getStringWidth(getPlayerName(networkplayerinfo));
+            nameLength = Math.max(nameLength, stringWidth);
+
+            if (scoreObjectiveIn != null && scoreObjectiveIn.getRenderType() != IScoreObjectiveCriteria.EnumRenderType.HEARTS) {
+                stringWidth = mc.fontRendererObj.getStringWidth(" " + scoreboardIn.getValueFromObjective(networkplayerinfo.getGameProfile().getName(), scoreObjectiveIn).getScorePoints());
+                objWidth = Math.max(objWidth, stringWidth);
+            }
+        }
+
+        list = list.subList(0, Math.min(list.size(), 80));
+        int listSize = list.size();
+        int columnHeight;
+        for (columnHeight = 1; listSize > TabulousConfig.overflow; listSize = (listSize + columnHeight - 1) / columnHeight) {
+            ++columnHeight;
+        }
+
+        boolean renderHeads = mc.isIntegratedServerRunning() || mc.getNetHandler().getNetworkManager().getIsencrypted();
+        if (TabulousConfig.dontShowHeads) renderHeads = false;
+        int objLength;
+
+        if (scoreObjectiveIn != null) {
+            if (scoreObjectiveIn.getRenderType() == IScoreObjectiveCriteria.EnumRenderType.HEARTS) {
+                objLength = 90;
+            } else {
+                objLength = objWidth;
+            }
+        } else {
+            objLength = 0;
+        }
+
+        int columnWidth = Math.min(columnHeight * ((renderHeads ? 9 : 0) + nameLength + objLength + 13), width - 50) / columnHeight;
+        int num1 = width / 2 - (columnWidth * columnHeight + (columnHeight - 1) * 5) / 2;
+        int top = TabulousConfig.topPosition;
+        int num2 = columnWidth * columnHeight + (columnHeight - 1) * 5;
+
+        List<String> headerList = null;
+        List<String> footerList = null;
+
+        if (getHeader() != null) {
+            headerList = mc.fontRendererObj.listFormattedStringToWidth(getHeader().getFormattedText(), width - 50);
+
+            for (String s : headerList) {
+                num2 = Math.max(num2, mc.fontRendererObj.getStringWidth(s));
+            }
+        }
+
+        if (getFooter() != null) {
+            footerList = mc.fontRendererObj.listFormattedStringToWidth(getFooter().getFormattedText(), width - 50);
+
+            for (String s : footerList) {
+                num2 = Math.max(num2, mc.fontRendererObj.getStringWidth(s));
+            }
+        }
+
+        // RENDER HEADER
+        if (headerList != null) {
+            Gui.drawRect(width / 2 - num2 / 2 - 1, top - 1, width / 2 + num2 / 2 + 1, top + headerList.size() * mc.fontRendererObj.FONT_HEIGHT, TabulousConfig.tabColor.getRGB());
+
+            for (String s : headerList) {
+                int i2 = mc.fontRendererObj.getStringWidth(s);
+                mc.fontRendererObj.drawStringWithShadow(s, (float) (width / 2 - i2 / 2), (float) top, -1);
+                top += mc.fontRendererObj.FONT_HEIGHT;
+            }
+
+            ++top;
+        }
+
+        Gui.drawRect(width / 2 - num2 / 2 - 1, top - 1, width / 2 + num2 / 2 + 1, top + listSize * 9, TabulousConfig.tabColor.getRGB());
+        for (int iterator = 0; iterator < listSize; ++iterator) {
+            int current = iterator / listSize;
+            int currentRemainder = iterator % listSize;
+            int currentLeft = num1 + current * columnWidth + current * 5;
+            int currentTop = top + currentRemainder * 9;
+            Gui.drawRect(currentLeft, currentTop, currentLeft + columnWidth, currentTop + 8, TabulousConfig.tabNameColor.getRGB());
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.enableAlpha();
+            GlStateManager.enableBlend();
+            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+
+            if (iterator < list.size()) {
+                NetworkPlayerInfo networkplayerinfo1 = list.get(iterator);
+                String name = getPlayerName(networkplayerinfo1);
+                GameProfile gameprofile = networkplayerinfo1.getGameProfile();
+
+                // RENDER HEAD
+                if (renderHeads) {
+                    EntityPlayer entityplayer = mc.theWorld.getPlayerEntityByUUID(gameprofile.getId());
+                    boolean hasCape = entityplayer != null && entityplayer.isWearing(EnumPlayerModelParts.CAPE) && (gameprofile.getName().equals("Dinnerbone") || gameprofile.getName().equals("Grumm"));
+                    mc.getTextureManager().bindTexture(networkplayerinfo1.getLocationSkin());
+                    int headTextureV = 8 + (hasCape ? 8 : 0);
+                    int headTextureSizeV = 8 * (hasCape ? -1 : 1);
+                    int headPos = currentLeft;
+                    if (TabulousConfig.headPos == 1) {
+                        headPos = currentLeft + columnWidth - 8;
+                    }
+                    Gui.drawScaledCustomSizeModalRect(headPos, currentTop, 8.0F, (float) headTextureV, 8, headTextureSizeV, 8, 8, 64.0F, 64.0F);
+
+                    if (entityplayer != null && entityplayer.isWearing(EnumPlayerModelParts.HAT)) {
+                        Gui.drawScaledCustomSizeModalRect(headPos, currentTop, 40.0F, (float) headTextureV, 8, headTextureSizeV, 8, 8, 64.0F, 64.0F);
+                    }
+
+                    if (TabulousConfig.headPos == 0) currentLeft += 9;
+                }
+
+                // RENDER NAMES
+                if (networkplayerinfo1.getGameType() == WorldSettings.GameType.SPECTATOR) {
+                    name = EnumChatFormatting.ITALIC + name;
+                    mc.fontRendererObj.drawStringWithShadow(name, (float) currentLeft, (float) currentTop, -1862270977);
+                } else {
+                    if (TabulousConfig.hideGuilds) {
+                        if (name.charAt(name.length() - 1) == ']') {
+                            name = name.substring(0, name.lastIndexOf("\u00A7"));
+                        }
+                    }
+                    mc.fontRendererObj.drawStringWithShadow(name, (float) currentLeft, (float) currentTop, -1);
+                }
+
+                if (scoreObjectiveIn != null && networkplayerinfo1.getGameType() != WorldSettings.GameType.SPECTATOR) {
+                    int rightName = currentLeft + nameLength + 1;
+                    int rightObj = rightName + objLength;
+
+                    if (rightObj - rightName > 5) {
+                        if (TabulousConfig.renderScoreboardValues) {
+                            renderScoreboardValues(scoreObjectiveIn, currentTop, gameprofile.getName(), rightName, rightObj, networkplayerinfo1);
+                        }
+                    }
+                }
+                if (TabulousConfig.renderPingNums && TabulousConfig.headPos != 1) {
+                    String pingNum = "\u00A7";
+                    int ping = networkplayerinfo1.getResponseTime();
+                    if (ping != 1) {
+                        if (ping < -1) pingNum += "5";
+                        else if (ping < 100) {
+                            pingNum += "2";
+                        } else if (ping < 300) {
+                            pingNum += "a";
+                        } else if (ping < 400) {
+                            pingNum += "e";
+                        } else {
+                            pingNum += "c";
+                        }
+                        pingNum += String.valueOf(networkplayerinfo1.getResponseTime());
+                        mc.fontRendererObj.drawStringWithShadow(pingNum, nameLength + currentLeft - (renderHeads ? 9 : 0) - mc.fontRendererObj.getStringWidth(pingNum), currentTop, -1);
+                    }
+                    return;
+                }
+                if (TabulousConfig.renderPing && TabulousConfig.headPos != 1)
+                    renderPing(nameLength, currentLeft - (renderHeads ? 9 : 0), currentTop, networkplayerinfo1);
+            }
+        }
+
+        // RENDER FOOTER
+        if (footerList != null) {
+            top = top + listSize * 9 + 1;
+            Gui.drawRect(width / 2 - num2 / 2 - 1, top - 1, width / 2 + num2 / 2 + 1, top + footerList.size() * mc.fontRendererObj.FONT_HEIGHT, TabulousConfig.tabColor.getRGB());
+
+            for (String s : footerList) {
+                int stringWidth = mc.fontRendererObj.getStringWidth(s);
+                mc.fontRendererObj.drawStringWithShadow(s, (float) (width / 2 - stringWidth / 2), (float) top, -1);
+                top += mc.fontRendererObj.FONT_HEIGHT;
+            }
+        }
+    }
 }
